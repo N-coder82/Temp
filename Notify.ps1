6b94fdce681a664305d5f1bef8803d81f2e481aa
NH <natey.hecht@gmail.com>
2023-01-27 17:16:23 -0800

    first commit

---
+++ Notify.ps1
@@ -0,0 +1,6 @@
+﻿[reflection.assembly]::loadwithpartialname("System.Windows.Forms")
+[reflection.assembly]::loadwithpartialname("System.Drawing")
+$notify = new-object system.windows.forms.notifyicon
+$notify.icon = [System.Drawing.SystemIcons]::Information
+$notify.visible = $true
+$notify.showballoontip(10,"Temp Cleared","Removed files from ""Temp"" Folder",[system.windows.forms.tooltipicon]::None)
---
+++ cleartemp.bat
@@ -0,0 +1,5 @@
+cd C:\path\to\delet\folder\placement\
+mkdir delfldr
+move C:\path\to\temp\folder\* C:\path\to\delete\folder
+rmdir /s /q C:\path\to\delete\folder
+"C:\path\to\powershell.exe" "C:\path\to\notify\script.ps1"
