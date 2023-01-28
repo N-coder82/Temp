bfb4306b56cd173ca765626d38c8450fd67a7c5e
NH <natey.hecht@gmail.com>
2023-01-27 17:26:47 -0800

    modified:   cleartemp.bat

--- cleartemp.bat
+++ cleartemp.bat
@@ -2,4 +2,4 @@ cd C:\path\to\delet\folder\placement\
 mkdir delfldr
 move C:\path\to\temp\folder\* C:\path\to\delete\folder
 rmdir /s /q C:\path\to\delete\folder
-"C:\path\to\powershell.exe" "C:\path\to\notify\script.ps1"
+"C:\path\to\powershell.exe" "C:\path\to\notify\script.ps1'
